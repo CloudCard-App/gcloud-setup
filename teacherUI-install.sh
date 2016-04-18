d497ff819a69231cd221b2b5ca2c7aab8b33ea57
Marc Bacvanski <marc.bacvanski@gmail.com>
2016-04-17 18:42:54 -0700

    Wrote teacherUI-install.sh

---
+++ teacherUI-install.sh
@@ -0,0 +1,31 @@
+# Written by Marc Bacvanski
+# DO NOT RUN THIS FILE BY ITSELF!!!
+# It's called by install.sh - run that one instead.
+
+echo "--------------------- Installing Node.js and NPM. ----------------------"
+
+sudo apt-get install -y nodejs npm
+
+echo "----------------- Finished installing Node.js and NPM. -----------------"
+
+mkdir ~/Servers
+cd ~/Servers/
+
+echo "--------------------- Created directory ~/Servers. ---------------------"
+
+git clone https://github.com/Flashcard-App/Flashcard-App-TeacherUI.git
+
+echo "----------- Cloned into ~/Servers/Flashcard-App-TeacherUI/. ------------"
+
+cd ~/Servers/Flashcard-App-TeacherUI/
+
+echo "-------------------- Installing Node.js libraries. ---------------------"
+
+npm install
+
+echo "---------------- Finished installing Node.js libraries. ----------------"
+echo "------------------------ Finishing installation. -----------------------"
+
+sudo chmod +x server.js
+
+echo "------------ All done! Run nodejs server.js to get started! ------------"
