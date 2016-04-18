fcd6bbf82f7477c9fe55cc7cb23000f97cf2259f
Marc Bacvanski <marc.bacvanski@gmail.com>
2016-04-17 18:29:16 -0700

    User Interface works now!

--- install.sh
+++ install.sh
@@ -8,22 +8,22 @@
 # sudo apt-get install nodejs npm vim 
 
 num=0
-until [$num -ge 1 -a $num -le 3]
+until ( [[ "$num" -ge 1 ]] && [[ "$num" -le 3 ]] )
 do
   echo "What type of server are you running?"
-  echo "[1]: TeacherUI
-  echo "[2]: Recorder
-  echo "[3]: MongoDB (by itself)
-  $num=read -n 1
+  echo "[1]: TeacherUI"
+  echo "[2]: Recorder"
+  echo "[3]: MongoDB (by itself)"
+  read num
 done
 
-if ["$num" == "1"]
+if ( [[ "$num" == '1' ]] ); then
   echo "Installing dependencies for TeacherUI type server."
   # Call the TeacherUI script
-elif ["$num" == "2"]
+elif ( [[ "$num" == '2' ]] ); then
   echo "Installing dependencies for Recorder type server."
   # Call the Recorder script
-elif ["$num" == "3"]
+elif ( [[ "$num" == '3' ]] ); then
   echo "Installing dependencies for MongoDB by itself."
   # Call the MongoDB script
 fi
